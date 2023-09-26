75e71c93771fc579a29e99809b2bcfe2a5167810
Vishnu Vardhan Chandragiri <chandragirivishnuvardhan@gmail.com>
2023-09-26 16:11:13 +0530

    bug fix

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-// export const server = "https://shop-o-backend.vercel.app/api/v2";
+export const server = "https://shop-o-backend.vercel.app/api/v2";
 // export const server = "http://localhost:8000/api/v2";
